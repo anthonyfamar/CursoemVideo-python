9f89958521ac79a9d0fbc430c9fc81b204cfd67e
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 15:30:21 -0300

    Update ex010.py

--- VideosTeorias/Video07/ex010.py
+++ VideosTeorias/Video07/ex010.py
@@ -1,5 +1,5 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.  Considere US$1,00 = R$3,27
 
-valor = float(input('Digite o valor que possui: '))
-dolar = valor * 3.27
-print(f'Vc poderá comprar US$ {dolar}')
+valor = float(input('Digite o valor que possui R$'))
+dolar = valor / 3.27
+print(f'Com R${valor}, vc poderá comprar US$ {dolar:.2f}')
