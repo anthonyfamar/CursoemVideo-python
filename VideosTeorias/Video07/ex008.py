4dc1b8c257b70cc5ac36b9a797628aaccb7a96dc
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 10:04:13 -0300

    Update ex008.py

--- VideosTeorias/Video07/ex008.py
+++ VideosTeorias/Video07/ex008.py
@@ -1,1 +1,5 @@
-# Escreva um programa que leia um valor em metros e o exiba conventido em centímetros e milímetros.
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+n1 = float(input('Digite um valor em metros: '))
+cm = n1 * 100
+mm = n1 * 1000
+print(f'O valor convertido é: \n{cm:.0f} centímetros \n{mm:.0f} milímetros')
