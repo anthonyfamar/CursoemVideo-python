c6bf0e1617f648a6981e2d409e0791b590007809
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 10:04:06 -0300

    Update ex006.py

--- VideosTeorias/Video07/ex006.py
+++ VideosTeorias/Video07/ex006.py
@@ -1,1 +1,6 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+n1 = int(input('Digite um número: '))
+d = n1 * 2
+t = n1 * 3
+r = n1 ** 2
+print(f'O dobro é {d:=^8} \nO triplo é {t:=^8} \nA raiz quadrada é {r:=^8}')
