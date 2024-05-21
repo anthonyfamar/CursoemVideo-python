6a62111e8ae4501416b6afeb09a7bab609f6f070
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 09:14:16 -0300

    Update ex005.py

--- VideosTeorias/Video07/ex005.py
+++ VideosTeorias/Video07/ex005.py
@@ -1,1 +1,5 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+n1 = int(input('Digite um número: '))
+ant = n1 - 1
+prox = n1 + 1
+print(f'O Antecessor é {ant} e o Sucessor é {prox}')
