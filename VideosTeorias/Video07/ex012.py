f132108525f19273fe5ebace9feea9ce080554d9
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 15:40:39 -0300

    Update ex012.py

--- VideosTeorias/Video07/ex012.py
+++ VideosTeorias/Video07/ex012.py
@@ -1,5 +1,4 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
-preco = float(input('Digite o preço do produto: '))
-desc = (5 * preco) / 100
-valor = preco - desc
-print(f'O valor final com desconto é R${valor}')
+preco = float(input('Digite o preço do produto R$ '))
+desc = preco - (5 * preco / 100)
+print(f'O valor final com desconto é R${desc:.2f}')
