b20f85a74e27e4cc4011518d1d060b4803fd3bdf
Anthony <anthonysouza75@hotmail.com.br>
2024-05-21 15:42:37 -0300

    Update ex013.py

--- VideosTeorias/Video07/ex013.py
+++ VideosTeorias/Video07/ex013.py
@@ -1,5 +1,4 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
-valor = float(input('Digite seu salário: '))
-aumento = (15 * valor) / 100
-salario = valor + aumento
-print(f'O seu novo salário é R${salario}')
+valor = float(input('Digite seu salário R$ '))
+aumento = valor + (15 * valor / 100)
+print(f'O seu novo salário com 15% de aumento é R${aumento}')
