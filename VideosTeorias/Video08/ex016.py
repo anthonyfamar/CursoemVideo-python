0720b85830410f1532b2f63c4b99fdf8a60492a6
Anthony <anthonysouza75@hotmail.com.br>
2024-06-18 16:26:56 -0300

    Update ex016.py

--- VideosTeorias/Video08/ex016.py
+++ VideosTeorias/Video08/ex016.py
@@ -1,2 +1,15 @@
-#Crie um programa que leia um número Real qualquer oelo teclado e mostre na tela a sua porção inteira.   
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.   
 # ex:Digite um número:6.127 o número tem a parte inteiro 6.
+from math import trunc
+num = float(input('Digite um número do tipo real: '))
+print(f'o número inteiro é: {trunc(num)}')
+
+
+
+
+
+
+#ou desse jeito:
+#  num = float(input('Digite um número do tipo real: '))
+#  trunc_num = trunc(num)
+#  print(f'o número inteiro é: {trunc_num}')
