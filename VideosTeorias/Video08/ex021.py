2fcff61e54eecf1a95ac32bd48b4169c92b04056
Anthony <anthonysouza75@hotmail.com.br>
2024-06-19 23:43:00 -0300

    Update ex021.py

--- VideosTeorias/Video08/ex021.py
+++ VideosTeorias/Video08/ex021.py
@@ -1,1 +1,7 @@
 #Faça um programa em python que abra e reproduza o audio de um arquivo MP3.
+import pygame
+pygame.mixer.init()
+pygame.init()
+pygame.mixer_music.load('D:\IMPORTANTE\Músicas\Drake - Toosie Slide.mp3')
+pygame.mixer_music.play()
+pygame.event.wait()
