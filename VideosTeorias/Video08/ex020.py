41c32fbfc9d1cde2c11af4a348c31264228d32e2
Anthony <anthonysouza75@hotmail.com.br>
2024-06-19 21:09:25 -0300

    Update ex020.py

--- VideosTeorias/Video08/ex020.py
+++ VideosTeorias/Video08/ex020.py
@@ -1,1 +1,10 @@
 # o mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+a1 = 'Anthony'
+a2 = 'Oda'
+a3 = 'Bruno'
+a4 = 'Gustavo'
+print(f'Os alunos que falta apresentar são os seguintes: {a1, a2, a3, a4}')
+alunos = [a1, a2, a3, a4]
+shuffle(alunos)
+print(f'A ordem de apresentação será: {alunos}')
