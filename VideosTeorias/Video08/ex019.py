1f107d04af0c123836a3d85ef3f6b8fa97569bbe
Anthony <anthonysouza75@hotmail.com.br>
2024-06-19 21:09:22 -0300

    Update ex019.py

--- VideosTeorias/Video08/ex019.py
+++ VideosTeorias/Video08/ex019.py
@@ -1,1 +1,10 @@
 #um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+a1 = 'Anthony'
+a2 = 'Oda'
+a3 = 'Bruno'
+a4 = 'Gustavo'
+a5 = 'David'
+print(f' Alunos: {a1, a2, a3, a4, a5}')
+alunos = choice([a1, a2, a3, a4, a5])
+print(f'O aluno que irá apagar o quadro é o {alunos}')
