79dcf3e6b2ff51f569e60cabffc32bb7d6bdcd16
Anthony <anthonysouza75@hotmail.com.br>
2024-06-25 15:04:11 -0300

    Create fatiamento.py

---
+++ VideosTeorias/Video09/fatiamento.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Video Python'
+print(frase[9]) #printa apenas o caracter 9
+print(frase[9:13]) #printa do 9 até o antecessor do 13
+print(frase[9:21]) #por mais q seja apenas 20 caracteres, pelo fato de sempre printar o antecessor esse da certo
+print(frase[9:21:2]) #printa do 9 ao 20, sendo um sim e outro não, pulando de 2
+print(frase[:5]) #printa do inicio ate o antecessor do 5
+print(frase[15:]) #printa do 15 até o final
+print(frase[9::3]) #começa printando do 9 e como não tem final entre os dois : ele vai até o final pulando de 3 em 3
