726b9459e1f3860292997593a553ea4a01305ba8
Anthony <anthonysouza75@hotmail.com.br>
2024-06-25 15:04:07 -0300

    Create divisão e junção.py

---
+++ VideosTeorias/Video09/divisão e junção.py
@@ -0,0 +1,3 @@
+frase = 'Curso em Video Python'
+print(frase.split()) # gera uma lista com as palavras na frase, divide a string em uma lista
+print('-'.join(frase)) #junta tudo mas com o traço separando
