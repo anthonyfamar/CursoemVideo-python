dd35d4ffa1018db667241894e91896dc697a40cd
Anthony <anthonysouza75@hotmail.com.br>
2024-07-08 16:23:04 -0300

    Update ex022.py

--- VideosTeorias/Video09/ex022.py
+++ VideosTeorias/Video09/ex022.py
@@ -2,3 +2,12 @@
 #- O nome com todas as letras maiúsculas e minúsculas.
 #- Quantas letras ao todo (sem considerar espaços).
 #- Quantas letras tem o primeiro nome.
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print(f'Seu nome completo tudo em maiúscula é: {nome.upper()}')
+print(f'Seu nome completo tudo em minúsculo é: {nome.lower()}')
+print(f'Quantidade de letras: {len(nome) - nome.count(' ')}')
+dividido = nome.split()
+print(f'O primeiro nome tem {len(dividido[0])} letras')
+#ou
+print(f'Seu primeiro nome tem {nome.find(' ')} letras')
