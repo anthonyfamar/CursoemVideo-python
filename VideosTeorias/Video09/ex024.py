e96f90b6f9aa1370fc77edce46c414ecb87805d4
Anthony <anthonysouza75@hotmail.com.br>
2024-07-08 19:43:39 -0300

    Update ex024.py

--- VideosTeorias/Video09/ex024.py
+++ VideosTeorias/Video09/ex024.py
@@ -1,1 +1,9 @@
 #Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO"
+cidade = str(input('Digite o nome da cidade: ')).strip()
+separado = cidade.split()
+print(f'A cidade começa com santo?? {'SANTO' in separado[0].upper()}')
+
+#ou
+
+#cid = str(input('Digite o nome da cidade: ')).strip()
+#print(cid[:5].upper() == 'SANTO')
