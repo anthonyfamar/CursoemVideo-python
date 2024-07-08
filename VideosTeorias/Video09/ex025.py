a07a78e0aebf585eee365b67c1c02ea4fb580f2a
Anthony <anthonysouza75@hotmail.com.br>
2024-07-08 20:26:38 -0300

    Update ex025.py

--- VideosTeorias/Video09/ex025.py
+++ VideosTeorias/Video09/ex025.py
@@ -1,1 +1,3 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome
+nome = str(input('Digite seu nome completo: ')).strip()
+print(f'Seu nome tem silva? {'SILVA' in nome.upper()}')
