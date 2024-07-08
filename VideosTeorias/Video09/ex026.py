062b9e6b30e9fedc21633ef9b333bbc78cb1470b
Anthony <anthonysouza75@hotmail.com.br>
2024-07-08 20:35:47 -0300

    Update ex026.py

--- VideosTeorias/Video09/ex026.py
+++ VideosTeorias/Video09/ex026.py
@@ -2,3 +2,7 @@
 # - quantas vezes aparece a letra "A" 
 # -em que posição ela aparece a primeira vez 
 # -e em que posição ela aparece a última vez.
+frase = str(input('Digite uma frase: ')).upper().strip()
+print(f'A letra "a" aparece {frase.count('A')} na frase!')
+print(f'A primeira letra A apareceu na posição {frase.find('A')+1}')
+print(f'A última letra A apareceu na posição {frase.rfind('A')+1}')
