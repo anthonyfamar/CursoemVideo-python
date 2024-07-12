15463f63acbccb0cc4a53c65a33f6a30e70d4a93
Anthony <anthonysouza75@hotmail.com.br>
2024-07-11 21:53:43 -0300

    Update ex028.py

--- VideosTeorias/Video10/ex028.py
+++ VideosTeorias/Video10/ex028.py
@@ -1,1 +1,8 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+from random import randint
+num = randint(0,5)
+n = int(input('Digite um número inteiro entre 0 e 5 que você acha que o computador irá escolher: '))
+if num == n:
+    print('Parabéns, você venceu!')
+else:
+    print(f'Você PERDEU, o número escolhido foi {num}, tente novamente!')
