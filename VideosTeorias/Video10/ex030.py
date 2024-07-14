c6313f849a856123a682c0f1b981667d231879a2
Anthony <anthonysouza75@hotmail.com.br>
2024-07-14 15:37:16 -0300

    Update ex030.py

--- VideosTeorias/Video10/ex030.py
+++ VideosTeorias/Video10/ex030.py
@@ -1,1 +1,7 @@
 #Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+num = int(input('Digite um número: '))
+resto = num % 2
+if resto==0:
+    print('O número que você escolheu é par!')
+else:
+    print('O número que você escolheu é impar!')
