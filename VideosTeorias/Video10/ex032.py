3256f81363bacf55cfc116ea8352052c6cf2ef1b
Anthony <anthonysouza75@hotmail.com.br>
2024-07-15 13:23:44 -0300

    Update ex032.py

--- VideosTeorias/Video10/ex032.py
+++ VideosTeorias/Video10/ex032.py
@@ -1,1 +1,6 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+ano = int(input('Digite um ano: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} É BISSEXTO!')
+else:
+    print(f'O ano {ano} NÃO É BISSEXTO!')
