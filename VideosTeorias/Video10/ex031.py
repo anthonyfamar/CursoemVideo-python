a91580bab49fb8c434000f3d9b44a3cf9274772f
Anthony <anthonysouza75@hotmail.com.br>
2024-07-15 13:23:42 -0300

    Update ex031.py

--- VideosTeorias/Video10/ex031.py
+++ VideosTeorias/Video10/ex031.py
@@ -1,1 +1,7 @@
 #Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
+km = float(input('Digite a distancia da viagem em KM: '))
+if km <= 200:
+    total = km * 0.50
+else:
+    total = km * 0.45
+print(f'O valor para sua viagem de {km}km ficou R${total:.2f}')
