dc4e443af171a760f913c23a6196069caef590f7
Anthony <anthonysouza75@hotmail.com.br>
2024-07-22 20:46:21 -0300

    Update ex019.py

--- VideosTeorias/Video08/ex019.py
+++ VideosTeorias/Video08/ex019.py
@@ -7,4 +7,4 @@ a4 = 'Gustavo'
 a5 = 'David'
 print(f' Alunos: {a1, a2, a3, a4, a5}')
 alunos = choice([a1, a2, a3, a4, a5])
-print(f'O aluno que irá apagar o quadro é o {alunos}')
+print(f'O aluno que irá apagar o quadro é o \033[1;37;41m{alunos}\033[m')
