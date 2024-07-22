3d333d64d916dd52b589025345609db789e6ff97
Anthony <anthonysouza75@hotmail.com.br>
2024-07-22 20:46:24 -0300

    Update ex028.py

--- VideosTeorias/Video10/ex028.py
+++ VideosTeorias/Video10/ex028.py
@@ -3,6 +3,6 @@ from random import randint
 num = randint(0,5)
 n = int(input('Digite um número inteiro entre 0 e 5 que você acha que o computador irá escolher: '))
 if num == n:
-    print('Parabéns, você venceu!')
+    print('\033[37;42mParabéns, você venceu!\033[m')
 else:
-    print(f'Você PERDEU, o número escolhido foi {num}, tente novamente!')
+    print(f'Você \033[41mPERDEU\033[m, o número escolhido foi {num}, tente novamente!')
