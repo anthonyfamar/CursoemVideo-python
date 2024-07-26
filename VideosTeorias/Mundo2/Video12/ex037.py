f224d2b8b4049d9ebad2986c32865ae0adf8c308
Anthony <anthonysouza75@hotmail.com.br>
2024-07-26 20:08:56 -0300

    Update ex037.py

--- VideosTeorias/Mundo2/Video12/ex037.py
+++ VideosTeorias/Mundo2/Video12/ex037.py
@@ -1,1 +1,16 @@
 #Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+num = int(input('Digite um número inteiro:'))
+print('Selecione a base que deseja converter: ')
+menu = int(input('1-Binário \n2-Octal \n3-Hexadecimal\n'))
+if menu == 1:
+    converte = bin(num)
+    print(f'O número convertido para a base binária é {converte[2:]}')
+elif menu == 2:
+    converte = oct(num)
+    print(f'O número convertido para a base octal é {converte[2:]}')
+elif menu == 3:
+    converte = hex(num)
+    print(f'O número convertido para a base hexadecimal é {converte[2:]}')
+else:
+    print('Escolha Inválida!')
+
