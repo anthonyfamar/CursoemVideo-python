ea4f7450049be21ee7d0fd341ab3c3871aa3f43b
Anthony <anthonysouza75@hotmail.com.br>
2024-07-26 20:08:54 -0300

    Update ex036.py

--- VideosTeorias/Mundo2/Video12/ex036.py
+++ VideosTeorias/Mundo2/Video12/ex036.py
@@ -1,1 +1,14 @@
 #Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+print('\033[1;32m-=\033[m'*10)
+print('   \033[1;32mBanco THONYN \033[m')
+print('\033[1;32m-=\033[m'*10)
+print('Seja bem vindo!')
+casa = float(input('Qual o valor da casa que deseja comprar? R$'))
+salario = float(input('Certo, e qual seria o valor do seu salário? R$'))
+tempo = int(input('E por último, em quantos anos deseja quitar ela? '))
+prestação = casa/(tempo*12)
+         
+if prestação > 0.30 * salario:
+    print('Financimento negado, pois o valor passou 30% do seu salário!')
+else:
+    print('Financiamento aprovado! PARABÉNS!')
