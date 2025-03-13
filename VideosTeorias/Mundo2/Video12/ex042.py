fe948e9e273230ca250e42945c5efca3c2184dd9
Anthony <anthonysouza75@hotmail.com.br>
2025-03-13 12:28:25 -0300

    Update ex042.py

--- VideosTeorias/Mundo2/Video12/ex042.py
+++ VideosTeorias/Mundo2/Video12/ex042.py
@@ -2,3 +2,20 @@
 #- EQUILÁTERO: todos os lados iguais
 #- ISÓSCELES: dois lados iguais, um diferente
 #- ESCALENO: todos os lados diferentes
+print('=-'*20)
+print('Analisador de triângulos')
+print('-='*20)
+a = float(input('Comprimento da primeira reta: '))
+b = float(input('Comprimento da segunda reta: '))
+c = float(input('Comprimento da terceira reta: '))
+#""se a soma das medidas de dois deles é sempre maior que a medida do terceiro, então, eles podem formar um triângulo"
+if a + b > c and b + c > a and a + c > b:
+    print('Pode formar um triângulo ', end='')
+    if a == b == c:
+        print("EQUILÁTERO!")
+    elif a != b != c != a:
+        print("ESCALENO!")
+    else:
+        print("ISÓSCELES!")
+else:
+    print('Não pode formar um triângulo!') 
