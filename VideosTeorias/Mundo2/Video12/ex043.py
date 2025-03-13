84dec0e60923c7861e5159fe12ed04ad604bb808
Anthony <anthonysouza75@hotmail.com.br>
2025-03-13 12:28:28 -0300

    Update ex043.py

--- VideosTeorias/Mundo2/Video12/ex043.py
+++ VideosTeorias/Mundo2/Video12/ex043.py
@@ -4,3 +4,18 @@
 #- 25 até 30: Sobrepeso
 #- 30 até 40: Obesidade
 #- Acima de 40: Obesidade Mórbida
+peso = float(input("Digite seu peso em kg: "))
+alt = float(input("Digite sua altura em metros:"))
+imc = peso/(alt ** 2)
+
+print(f"Seu IMC é {imc:.2f}kg/m², está ", end='')
+if imc < 18.5:
+    print("ABAIXO DO PESO!")
+elif 18.5 <= imc < 25:
+    print("no PESO IDEAL!")
+elif 25 <= imc < 30:
+    print("com SOBREPESO!")
+elif 30 <= imc < 40:
+    print("com OBESIDADE!")
+elif imc >= 40:
+    print("grave, OBESIDADE MÓRBIDA!")  
