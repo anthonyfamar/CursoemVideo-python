a1628bf33506cb39daa049eac9ee721b68cb5ac9
Anthony <anthonysouza75@hotmail.com.br>
2025-03-13 09:52:29 -0300

    att

--- VideosTeorias/Mundo2/Video12/ex040.py
+++ VideosTeorias/Mundo2/Video12/ex040.py
@@ -2,3 +2,14 @@
 #- Média abaixo de 5.0: REPROVADO
 #- Média entre 5.0 e 6.9: RECUPERAÇÃO
 #- Média 7.0 ou superior: APROVADO
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input("Digite sua segunda nota: "))
+media = (nota1 + nota2)/2
+
+if media < 5:
+    print(f"REPROVADO! Sua média é {media:.2f}")
+elif 5 < media <6.9:
+    print(f"RECUPERAÇÃO! Sua média é {media:.2f}")
+else:
+    print(f"APROVADO! Sua média é {media:.2f}")
--- VideosTeorias/Mundo2/Video12/ex041.py
+++ VideosTeorias/Mundo2/Video12/ex041.py
@@ -4,3 +4,17 @@
 #- Até 19 anos: JÚNIOR
 #- Até 25 anos: SÊNIOR
 #- Acima de 25 anos: MASTER
+atual = int(input("Digite o ano atual: "))
+nascimento = int(input("Digite seu ano de nascimento: "))
+idade = atual - nascimento
+
+if idade <= 9:
+    print(f"Sua categoria é MIRIM!")
+elif 9 < idade <=14 :
+    print(f"Sua categoria é INFANTIL!")
+elif 14 < idade <=19:
+    print(f"Sua categoria é JÚNIOR!")
+elif 19 < idade <= 25:
+    print("Sua categoria é SÊNIOR!")
+else:
+    print("Sua categoria é MASTER!")
